15530474931ea3d8cd286ea422844abc5a496c63
Nagendra Y <nagynv@gmail.com>
2019-09-15 18:04:37 +0530

    Update test.py

--- test.py
+++ test.py
@@ -42,9 +42,9 @@ api.set_appname(APP_ID)
 def send_text_message(to,message):
         #api is the Mesibo Python API instance. Make sure the instance is initialised before you call API functions
         p = {}
-        p["peer"] = to
-        p["expiry"] = 3600
-        p["flag"] = MESIBO_FLAG_DELIVERYRECEIPT | MESIBO_FLAG_READRECEIPT
+        p['peer'] = to
+        p['expiry] = 3600
+        p['flag'] = MESIBO_FLAG_DELIVERYRECEIPT | MESIBO_FLAG_READRECEIPT
         data = str(message)
         api.send_message(p,api.random(),data)
 
